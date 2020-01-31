8a01a410c149ecb79525a9ae0b103d1632c1ab03
Sam Hecht <s013401@students.lmsd.org>
2020-01-31 14:24:56 -0500

    Added player 1

---
+++ GitHubExample/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  GitHubExample
+//
+//  Created by Samuel Hecht (student LM) on 1/31/20.
+//  Copyright © 2020 Samuel Hecht (student LM). All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+   
+    override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        let player1 = SKSpriteNode(imageNamed: "player 1")
+        player1.setScale(0.5)
+        player1.position = CGPoint.zero 
+        player1.zPosition = 1.0
+        addChild(player1)
+    }
+    
+
+}
