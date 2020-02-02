abca6b4948e0949823759d96924cfec32fa19dff
Sam Hecht <s013401@students.lmsd.org>
2020-02-01 22:35:23 -0500

    Deleted Player 2

--- GitHubExample/GameScene.swift
+++ GitHubExample/GameScene.swift
@@ -23,12 +23,9 @@ class GameScene: SKScene {
         
         
         
+
         
-        let player2 = SKSpriteNode(imageNamed: "player 2")
-        player2.setScale(0.5)
-        player2.position = CGPoint(x: 0, y: 100)
-        player2.zPosition = 1.0
-        addChild(player2)
+        
     }
     
 
